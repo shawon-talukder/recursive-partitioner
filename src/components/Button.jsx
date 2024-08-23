7325cbea20e7e79a099d050e100fbed9b9d5aca9
shawon talukder <shawontalukder.study@gmail.com>
2024-08-23 17:25:23 +0600

    chore: button

---
+++ src/components/Button.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+
+const Button = ({ label, handleClick, isDisabled, classes }) => {
+    const className = `px-4 py-2 text-sm font-medium text-gray-900 bg-white border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white ${classes} disabled:cursor-not-allowed disabled:bg-neutral-300`
+    
+    return (
+        <button
+            type="button"
+            onClick={handleClick}
+            disabled={isDisabled}
+            className={className}
+        >
+            {label}
+        </button>
+    )
+}
+
+Button.propTypes = {
+    label: PropTypes.string.isRequired,
+    handleClick: PropTypes.func.isRequired,
+    isDisabled: PropTypes.bool,
+    classes: PropTypes.string
+};
+
+export default Button
