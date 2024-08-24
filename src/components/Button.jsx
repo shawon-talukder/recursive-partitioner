0093186264affdf4a6786b21245f53a0debac897
shawon talukder <shawontalukder.study@gmail.com>
2024-08-24 23:50:20 +0600

    style: change style for remove button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
 const Button = ({ label, handleClick, isDisabled, classes }) => {
-    const className = `px-4 py-2 text-sm font-medium text-gray-900 bg-white border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white ${classes} disabled:cursor-not-allowed disabled:bg-neutral-300`
+    const className = `px-4 py-2 text-sm font-medium text-gray-900 bg-white border-gray-200 hover:bg-gray-100 hover:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 ${classes} disabled:cursor-not-allowed disabled:bg-neutral-300 dark:disabled:bg-neutral-700 `
     
     return (
         <button
